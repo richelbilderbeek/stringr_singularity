6aca7147cba56f7f43c09744b0abd5f594f6644e
richelbilderbeek <richel@richelbilderbeek.nl>
2022-02-24 12:51:48 +0100

    Fix demo

--- scripts/demo_container.R
+++ scripts/demo_container.R
@@ -1,7 +1,1 @@
-library(plinkr)
-plink_optionses <- create_plink_optionses(plink_folder = "/opt/plinkr")
-assoc(
-  assoc_params = create_demo_assoc_params(),
-  plink_options = plink_optionses[[2]]
-)
-
+library(stringr)
